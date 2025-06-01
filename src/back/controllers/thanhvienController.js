b84813aa5ba47baa802bc59cd6a2f35516910113
100-KG <167777232+100-KG@users.noreply.github.com>
2025-06-01 22:00:00 +0700

    Update back #2

--- src/back/controllers/thanhvienController.js
+++ src/back/controllers/thanhvienController.js
@@ -1,15 +1,17 @@
-import { checkLogin } from "../dbservices/thanhvienFunction";
+import { checkLogin } from "../dbservices/thanhvienFunction.js";
 
 exports.login = async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await checkLogin(username, password);
         if (user) {
-            // Trả về role để client quyết định tải trang nào
             res.status(200).send({ message: 'Login successful', role: user.role, user });
         } else {
             res.status(401).send({ error: 'Invalid username or password' });
         }
+        const token = jwt.sign({
+            user
+        })
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: 'Login failed' });
@@ -18,13 +20,11 @@ exports.login = async (req, res) => {
 exports.register = async (req, res) => {
     const { username, password, role } = req.body;
     try {
-        // Kiểm tra xem người dùng đã tồn tại chưa
         const existingUser = await checkLogin(username, password);
         if (existingUser) {
             return res.status(400).send({ error: 'Username already exists' });
         }
 
-        // Thêm người dùng mới vào cơ sở dữ liệu
         const [result] = await pool.query(`
             INSERT INTO tblThanhVien (username, password, role)
             VALUES (?, ?, ?)
--- src/back/routers/thanhvienRouter.js
+++ src/back/routers/thanhvienRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const thanhvienController = require('./back/controllers/thanhvienController.js');
+const thanhvienController = require('./src/back/controllers/thanhvienController.js');
 const router = express.Router();
 
 router.post('/login', thanhvienController.login);
-router.post('/register', thanhvienController.register);
+// router.post('/register', thanhvienController.register);
 
 module.exports = router;
